860ac83949880a2164bd0663d87947d1934a9caa
Naveli Pangal <navelipangal649@gmail.com>
2025-11-02 14:39:56 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
+import path from "path";
 
 
 dotenv.config(); // load .env variables
 const app = express(); //get an app instance form express
+const _dirname = path.resolve();
 
 const PORT = process.env.PORT;
 
@@ -17,7 +19,15 @@ const PORT = process.env.PORT;
 //     res.send("Signup point");
 // })
 
-app.use("api/auth",authRoutes);
+app.use("/api/auth",authRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(_dirname,"../frontend","dist","index.html"));
+    })
+}
 app.listen(3000,() => console.log("Server running on port 3000:"+ PORT));
 
 
